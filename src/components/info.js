5f29ed6bf5f822d358f0a6bcbceebe77df09518a
vitalikdrobot <vitalik@active-bridge.com>
2019-04-16 14:01:19 +0300

    Creating an info component

---
+++ src/components/info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Info extends React.Component {
+  render() {
+    return(
+      <div>
+        <h2>Погодний додаток</h2>
+        <p>Дізнатися погоду у вашому місті!</p>
+      </div>
+    );
+  }
+}
+
+export default Info;
