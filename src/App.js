e9c23cc5b32ea08a3fdd1c1a016a1c21f4d013e7
vitalikdrobot <vitalik@active-bridge.com>
2019-04-16 14:08:18 +0300

    Create and connect the main components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Info from './components/info';
+import Form from './components/form';
+import Weather from './components/Weather';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Info />
+        <Form />
+        <Weather />
       </div>
     );
   }
